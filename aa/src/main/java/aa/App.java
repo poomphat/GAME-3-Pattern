d2eece2cf225de3753db0097fddc50e1b25e19cf
SoravitYK <42952008+SoravitYK@users.noreply.github.com>
2020-08-24 21:44:28 +0700

    add GUI

--- aa/src/main/java/aa/App.java
+++ aa/src/main/java/aa/App.java
@@ -1,94 +1,220 @@
 package aa;
 
+
+
 import java.util.*;
+import java.awt.FlowLayout;
+import java.awt.Graphics;
+import java.awt.event.*;
+import java.io.IOException;
+
+import javax.swing.*;
 
 /**
  * Hello world!
  *
  */
-public class App {
-	static boolean playing = true;
-	private static Scanner scanner;
-    public static void main(String[] args) {   
+public class App extends JPanel implements ActionListener{
+	static boolean playing = true,IsDead=false;
+	JButton attack,heal,exit,start;
+	
+	static Hero hero = Hero.GetHero();
+	static Monster mon1 = new Monster();
+	static EnemyPool pool = new EnemyPool();
+	static int nmonster=2;
+	static int count=0;
+	static Boss boss=pool.borrow();
+	private static String att = "";static Monster monster = mon1.clone();
+	static boolean firstTime=true,firstTimeBoss=true;
+	public App() {
+		super();
+		start=new JButton("start");
+		 attack = new JButton("Attack");
+		 heal = new JButton("Heal");
+		 exit = new JButton("Exit");
+		 start.addActionListener(this);
+		attack.addActionListener(this);     
+        heal.addActionListener(this);
+        exit.addActionListener(new ActionListener() {
+        	@Override
+			public void actionPerformed(ActionEvent e) { System.exit(0);; }
+		});
+        add(start);
+        add(attack);
+        add(heal);
+        add(exit);
+        setLayout(new FlowLayout(FlowLayout.CENTER));
+	        //}
+	   
+	    
+	   
+		//super();
+		
+	}
+	public void paintComponent(Graphics g0) {
+		
+	}
+	
+    public static void main(String[] args) throws IOException {   
         
+    	JFrame frame = new JFrame();
+        frame.getContentPane().add(new App());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setBounds(100, 100, 640, 600);
+        frame.setTitle("Gamenaja");
+        frame.setVisible(true);
         System.out.println("Hello");
-        scanner = new Scanner(System.in);
-        Hero hero = Hero.GetHero();
-        Monster mon1 = new Monster();
-        EnemyPool pool = new EnemyPool();
-        while(playing) {
-	        for (int i = 0; i < pool.getMaxPool(); i++) {
+        //Scanner scanner = new Scanner(System.in);
+       
+    }
+ 
+//}
+    @Override
+	    public void actionPerformed(ActionEvent e) {
+    	
+        if(playing) {
+        	if(!IsDead) {											//Is dead=false 
+        	if(count<3) {											
 	            if (hero.getHp() == 0) {
 	            	System.out.println("Game Over!!!");
-	            	break;
+	            	
 	            }
-	            
-	            for (int o = 0;o<2;o++) {
-	        		Monster monster = mon1.clone();
-	                System.out.println("Found Monster : Hp is " + monster.getHp());
+	           // System.err.println("count = "+count);
+	      		
+	            if(nmonster>0) {
 	                
-	                while (monster.getHp() != 0) {
-	                	System.out.println("What do you do : Attack(A) Heal(H)");
-	                    String att = scanner.nextLine();
-	                    if (att.equals("A")) {
-	                        hero.Attack(monster);
-	                        System.out.println("Monster Hp now is " + monster.getHp());
-	                        monster.Attack(hero);
-	                        System.out.println("Hero Hp now is " + hero.getHp());
+	            	if(firstTime) {
+	            		showMonster();
+	            		
+	                firstTime=false;
+	            	}
+	                if(monster.getHp() > 0) {					
+	                    //String att = scanner.nextLine();
+	                	//while(att.equals(""))
+	                    if (e.getSource().equals(attack)) {
+	                    	attMonster();
+	                    	checkMonDead();
+	                    	checkHeroHP();
 	                        
 	                    }
-	                    else if (att.equals("H")) {
-	                    	hero.heal();
-	                        System.out.println("Heal!!!!!!!, Hero Hp now is " + hero.getHp());
-	                    }
-	                    if(monster.getHp() == 0)
-	                    {
-	                    	System.out.println("!!!Monster has been defeated!!!");
+	                    else if (e.getSource().equals(heal)) {
+	                    	heroHeal();
 	                    }
 	                    
 	               }
-	                
-	            }
-	
-	            Boss boss = pool.borrow(); 
-	            System.out.println("Found Boss : Hp is " + boss.getHp());
-	                
-	            while (boss.getHp() != 0) {
-	            	System.out.println("What do you do : Attack(A) Heal(H)");
-	                String att = scanner.nextLine();
-	                if (att.equals("A")) {
-	                	hero.Attack(boss);
-	                	System.out.println("Boss Hp now is " + boss.getHp());
-	                	if (hero.getHp() == 0) {
-	                		break;
-	                	}
-	                }
-	                if (att.equals("H")) {
-	                	hero.heal();
-	                	System.out.println("Heal!!!!!!!, Hero Hp now is " + hero.getHp());
-	                }
-	                if(boss.getHp() == 0)
-	                {
-	                	System.out.println("!!!BOSS has been defeated!!!");
-	                }
-	            }
-	            pool.back(boss);
+	            
+	            }	            
+	            else if(boss.getHp()>0&&nmonster==-1) {	
+	            	
+	            	 if (e.getSource().equals(attack)) {
+	            		 attBoss();
+	            		 checkBoss(); 
+	            		 }
+	            	 
+	            	 checkHeroHP();
+	            	 
+	            	 if (e.getSource().equals(heal)) {
+	            		 heroHeal();
+                }
+	            }/*else if(nmonster==-1||boss.getHp()<=0){
+	            	pool.back(boss);
+	            	nmonster=2;
+	            	count++;
+	            	firstTimeBoss=true;
+	            } */
 	           
-	        }
-	        if (hero.getHp() > 0) {
-	        	System.out.println("You win!!!");
-	        }
-	        System.out.println("Play again (P) or End (Some key)");
-	        String key = scanner.nextLine();
-	        if (key.equals("P")) {
-	        	hero.Hp = 100;
-	        	playing = true;
-	        }else {
-	        	playing = false;
-	        	System.out.println("Goodbye");
-	        }
         }
-
-
+	            
+}	
+        	else {													// dead (Isdead = true)
+        		System.out.println("game is over!!! press start or exit");
+        		if(e.getSource().equals(start)) {
+        			startNew();
+        			showMonster();
+        		}
+}
+        	
+        }
+      //  System.err.println("Bug??? naniiiiiiiiiiiii");
     }
+    void showMonster() {
+    	if(nmonster>=1) {
+    	System.out.println("Found Monster : Hp is " + monster.getHp());
+    	System.out.println("Hero Hp now is " + hero.getHp());
+        System.out.println("What do you do : Attack(A) Heal(H)");
+        }
+    }
+    void attMonster() {
+    	 hero.Attack(monster);
+         System.out.println("Monster Hp now is " + monster.getHp());
+         monster.Attack(hero);
+         System.out.println("Hero Hp now is " + hero.getHp());
+    }
+    void heroHeal() {
+    	hero.heal();
+        System.out.println("Heal!!!!!!!, Hero Hp now is " + hero.getHp());
+    }
+    void showBoss() {
+    	
+    	System.out.println("Found Boss : Hp is " + boss.getHp());
+    	System.out.println("Hero Hp now is " + hero.getHp());
+        System.out.println("What do you do : Attack(A) Heal(H)");
+    }
+    void startNew() {
+    	System.out.println("Game Start!!!");
+    	IsDead=false;
+    	hero.Hp=100;
+    	nmonster=2;
+    	count=1;
+    	
+    }
+    void gameOver() {
+    	System.err.println("Game over");
+    	IsDead=true;
+    }
+    void attBoss() {
+    	  hero.Attack(boss);
+          System.out.println("Boss Hp now is " + boss.getHp());
+          boss.Attack(hero);
+          System.out.println("Hero Hp now is " + hero.getHp());
+    }
+    void checkMonDead() {
+    	if(monster.getHp() <= 0 && nmonster>0) {
+      	  nmonster--;monster = mon1.clone();   
+  	      //System.err.println("nmonter = " + nmonster);
+  	      System.out.println("Wow Monster Defeated");
+  	      showMonster();   
+  	      checkLastMon();
+      }
+    }
+    void checkLastMon() {			//check nmonsters are 2? boss start:nothing; 
+    	if(nmonster==0) {
+      		boss = pool.borrow();
+      		showBoss();
+      		nmonster=-1;
+      		}
+    }
+    void checkHeroHP() {
+    	if (hero.getHp() == 0) {
+      		gameOver();
+        }
+    }
+    void checkBoss() {				//check boss dead
+    	if(boss.getHp()<=0) {
+			 pool.back(boss);
+        	nmonster=2;
+        	count++;
+        	firstTimeBoss=true;
+        	checkLastBoss();
+    }
+    	}
+    void checkLastBoss() {			// there are last boss??
+    	if(count<3) {
+         	showMonster();
+		 }else {
+			 System.out.println("Win!!!");
+			 gameOver();
+		 }
+    }
+
 }
